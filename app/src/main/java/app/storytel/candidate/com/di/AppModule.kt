d9d2400c9b12e6176ee7f52695291ebf02a4166d
yalematta <layale.matta@gmail.com>
2021-06-19 14:17:30 +0300

    add AppModule

---
+++ app/src/main/java/app/storytel/candidate/com/di/AppModule.kt
@@ -0,0 +1,28 @@
+package app.storytel.candidate.com.di
+
+import app.storytel.candidate.com.network.ApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(ApiService.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideApiService(retrofit: Retrofit): ApiService =
+        retrofit.create(ApiService::class.java)
+}
